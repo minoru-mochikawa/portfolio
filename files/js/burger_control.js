5ccdfd9219c9a35041fe207a14d2ff0242e2638b
Minoru Mochikawa <minomino9265@gmail.com>
2022-01-01 19:17:38 +0900

    フッターのバグを修正

--- files/js/burger_control.js
+++ files/js/burger_control.js
@@ -5,7 +5,7 @@ $(document).on("click", ".burger-btn", function() {
     $("body").toggleClass("noscroll"); //bodyにnoscrollクラスを付与(スクロールを固定)
 });
 
-$(document).on("click", ".nav-link", function() {
+$(document).on("click", ".header .nav-link", function() {
     console.log(".burger-btn is clicked ");
     //.btn_triggerをクリックすると
     $(".burger-btn").toggleClass("close"); //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
