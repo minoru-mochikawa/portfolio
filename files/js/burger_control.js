9e264e33d5d4f7e667d98c54226c2b010765c1c2
Minoru Mochikawa <minomino9265@gmail.com>
2022-01-01 09:52:15 +0900

    headerとnavのデザイン変更

---
+++ files/js/burger_control.js
@@ -0,0 +1,14 @@
+$(document).on("click", ".burger-btn", function() {
+    //.btn_triggerをクリックすると
+    $(".burger-btn").toggleClass("close"); //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+    $(".nav-wrapper").toggleClass("fade"); //.nav-wrapperに
+    $("body").toggleClass("noscroll"); //bodyにnoscrollクラスを付与(スクロールを固定)
+});
+
+$(document).on("click", ".nav-link", function() {
+    console.log(".burger-btn is clicked ");
+    //.btn_triggerをクリックすると
+    $(".burger-btn").toggleClass("close"); //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+    $(".nav-wrapper").toggleClass("fade"); //.nav-wrapperに
+    $("body").toggleClass("noscroll"); //bodyにnoscrollクラスを付与(スクロールを固定)
+});
